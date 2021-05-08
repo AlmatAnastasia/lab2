1fb2fef5e739d5d24583347f27657d69724455a4
AlmatAnastasia <78953014+AlmatAnastasia@users.noreply.github.com>
2021-05-08 18:24:18 +0300

    Add files via upload

--- task2/task2.c
+++ task2/task2.c
@@ -3,8 +3,6 @@
 #include <math.h>
 
  double fun(double x)
-{
-     double f;
-     x <= 3 ? (f=pow(x, 2) - 3 * x + 9):  (f=1/(pow(x, 3) + 6));  
-     return(f);
+{ 
+     return x <= 3 ? (pow(x, 2) - 3 * x + 9):  (1/(pow(x, 3) + 6));  ;
 }
